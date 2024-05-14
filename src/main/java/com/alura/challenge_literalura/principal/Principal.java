ff7f89c28fdc20581dfaa30a1c9bf5b6fcc93604
JCarlos <dev_solsoft@aol.com>
2024-05-14 09:59:31 -0600

    agregando menu

---
+++ src/main/java/com/alura/challenge_literalura/principal/Principal.java
@@ -0,0 +1,48 @@
+package com.alura.challenge_literalura.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    private final Scanner teclado = new Scanner(System.in);
+
+
+    public void muestraElMenu() {
+        var opcion = -1;
+        while (opcion != 0) {
+            var menu = """
+                    
+                    Bienvenido a LiterAlura!
+                    
+                    1 - Buscar libro por titulo 
+                    2 - Listar libros registrados
+                    3 - Listar autores registrados
+                    4 - Listar autores vivos de un determinado año
+                    5 - Listar libros por idioma
+                                  
+                    0 - Salir
+                    """;
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 0:
+                    System.out.println("Cerrando la aplicación...");
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }
+
+    }
+}
